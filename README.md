03c61a2423c1445bdec51bd000b58f6d887042eb
Luca Sandrock <luca.sandrock@proton.me>
2025-05-05 00:55:43 +0200

    feat: first window positioning

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
   <h1>wayland-boomer</h1>
   <h3>zoomer application for linux</h3>
 </div>
+
+WARNING: This is work in progress and not yet ready for use.
--- src/main.c
+++ src/main.c
@@ -3,8 +3,36 @@
 int main(void) {
   SetTraceLogLevel(LOG_INFO);
 
-  SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+  SetConfigFlags(FLAG_BORDERLESS_WINDOWED_MODE | FLAG_WINDOW_RESIZABLE | FLAG_WINDOW_UNDECORATED | FLAG_WINDOW_TOPMOST);
   InitWindow(1080, 720, "wayland-boomer");
+  SetWindowFocused();
+
+  TraceLog(LOG_INFO, "Monitor count: %d", GetMonitorCount());
+  for (int monitor_idx = 0; monitor_idx < GetMonitorCount(); monitor_idx++) {
+    Vector2 monitor_position = GetMonitorPosition(monitor_idx);
+    TraceLog(
+        LOG_INFO,
+        "Monitor %d: %s (%dx%d) @ (%d, %d)",
+        monitor_idx,
+        GetMonitorName(monitor_idx),
+        GetMonitorWidth(monitor_idx),
+        GetMonitorHeight(monitor_idx),
+        (int)monitor_position.x,
+        (int)monitor_position.y
+    );
+  }
+
+  // https://www.glfw.org/docs/3.3/intro_guide.html#coordinate_systems (glfw is default raylib backend)
+  // since wayland has no such concept as a primary monitor, and doesnt allow clients to place themselves at specific
+  // coordinates, i dont see any non-hacky way to find the top left corner of all monitors. raylib programs (as of now)
+  // run via xwayland so we have window positioning, introducing a flag to have the user specify the leftmost monitor
+  // should be enough to use SetWindowPosition() to place the window correctly until wayland itself figures this out.
+  // for now we display the window at GetMonitorPosition(1) if there are multiple monitors. at least for my setup,
+  // this displayed the window at the top left corner of the leftmost monitor as wanted.
+  // TODO: introduce flag
+  int     monitor_idx      = GetMonitorCount() == 1 ? 0 : 1;
+  Vector2 monitor_position = GetMonitorPosition(monitor_idx);
+  SetWindowPosition((int)monitor_position.x, (int)monitor_position.y);
 
   SetTargetFPS(60);
   while (!WindowShouldClose()) {
