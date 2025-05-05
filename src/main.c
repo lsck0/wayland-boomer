1b6f0923d8648df47e4544cb92e2c06440815e74
Luca Sandrock <luca.sandrock@proton.me>
2025-05-06 00:17:15 +0200

    feat: prototype panning

--- src/main.c
+++ src/main.c
@@ -17,16 +17,27 @@ int main(void) {
   init_raylib_window(img.width, img.height, was_file);
 
   Texture2D       img_texture        = LoadTextureFromImage(img);
-  RenderTexture2D img_render_texture = LoadRenderTexture(img.width, img.height);
+  RenderTexture2D img_render_texture = LoadRenderTexture(2 * img.width, 2 * img.height);
   UnloadImage(img);
+
+  Vector2 panning = {0, 0};
 
   SetTargetFPS(60);
   while (!WindowShouldClose()) {
     if (IsKeyPressed(KEY_Q) || IsKeyPressed(KEY_ESCAPE)) break;
+    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+      // BUG: raylib's GetMousePosition() is broken when the window is on multiple monitors
+      Vector2 d  = GetMouseDelta();
+      panning.x += d.x;
+      panning.y += d.y;
+
+      // Vector2 mouse_pos = GetMousePosition();
+      // TraceLog(LOG_INFO, "Mouse position: %f %f", mouse_pos.x, mouse_pos.y);
+    }
 
     BeginTextureMode(img_render_texture);
     ClearBackground(BLACK);
-    DrawTextureEx(img_texture, (Vector2){0}, 0.0F, 1.0F, WHITE);
+    DrawTextureEx(img_texture, panning, 0.0F, 1.0F, WHITE);
     EndTextureMode();
 
     BeginDrawing();
